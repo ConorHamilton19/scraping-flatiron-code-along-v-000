a19d09a4fe12046b03a919a1f955967465aa35a3
Conor Hamilton <conhamil19@gmail.com>
2018-05-30 04:47:17 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Course 
+  attr_accessor :name, :schedule, :description
+  @@all = []
+  
+  def initialize
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
 end
 
